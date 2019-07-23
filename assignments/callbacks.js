bdcb9d4d6ad26f21758379a12654474a5af0cf88
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-07-23 17:43:23 -0500

    reworked a callback challenge

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -63,18 +63,19 @@ console.log(multiplyNums(4,8, function(product){
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-  for (i = 0 ; i < list.length ; i ++) {
-    if(list[i] === item) {
-      return cb(true)
-    }
+  let foundItem = list.filter(function(element){
+    if(element === item) {
+      return true
+    } 
     else {
-      return cb(false);
+      return false
     }
-  }
+  });
+  return cb(foundItem);
 }
     
 
-console.log(contains('Pencil', items, function(containsItem){
+console.log(contains('pie', items, function(containsItem){
   return containsItem;
 }))
 
